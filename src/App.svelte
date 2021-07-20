e584b75adb2707587d8a70646b7ec8292beb5c40
VikyLin <Airilet@yandex.ru>
2021-07-20 22:23:20 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Tailwind from './components/Tailwind.svelte';
+
+  export let name;
+</script>
+
+<Tailwind />
+
+<main class="text-center p-4 max-w-xs mx-auto sm:max-w-none">
+  <h1 class="text-red-500 text-6xl uppercase leading-loose font-thin">Hello {name}!</h1>
+  <p class="text-gray-700">
+    Visit the <a class="text-blue-600 hover:underline" href="https://svelte.dev/tutorial"
+      >Svelte tutorial</a
+    > to learn how to build Svelte apps.
+  </p>
+</main>
